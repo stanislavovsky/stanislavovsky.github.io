ba2b6a0e7dead849841fa2fe01e21756d4b52533
isinia <181978133+isinia@users.noreply.github.com>
2024-10-20 12:37:33 +0200

    fix: extend Window in TypeScript definition

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
+
+import { type Container, type Engine } from "tsparticles-engine";
+
+export {};
+
+declare global {
+  interface Window {
+    particlesInit: (engine: Engine) => Promise<void>;
+    particlesLoaded: (container: Container) => void;
+  }
+}
